62e6a48672e2b586c91397b95ed726e0f419e697
Matheus <matheusolivb16@gmail.com>
2025-07-02 18:31:37 -0300

    Primeiro commit do site LetCordeiroNails

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+document.getElementById("form-agendamento").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Agendamento enviado com sucesso! Entraremos em contato pelo WhatsApp.");
+  });
+  
